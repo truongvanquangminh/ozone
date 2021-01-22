49de55308d9ab7a58c6094c0aec1b5fee9b00b36
truongvanquangminh <minhtruong20002@gmail.com>
2021-01-23 00:15:22 +0700

    Edit 12h00PM

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@ var mybutton = document.getElementById("myBtn");
 window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
-  if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
+  if (document.body.scrollTop > 800 || document.documentElement.scrollTop > 800) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
